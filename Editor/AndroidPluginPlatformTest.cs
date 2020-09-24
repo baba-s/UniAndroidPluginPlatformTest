09e31c225cf53ddcc7df37add5d94630c6665263
baba_s <shotababa1206@gmail.com>
2020-09-24 19:53:04 +0900

    first commit

---
+++ Editor/AndroidPluginPlatformTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal sealed class AndroidPluginPlatformTest
+	{
+		[Category( nameof( Kogane ) )]
+		[Test]
+		public void Android_プラグインのプラットフォームが適切か()
+		{
+			var list = AssetDatabase
+					.GetAllAssetPaths()
+					.Where( c => c.Contains( "/Plugins/" ) )
+					.Where( c => c.Contains( "/Android/" ) )
+					.Select( c => AssetImporter.GetAtPath( c ) )
+					.OfType<PluginImporter>()
+					.Where( c => c != null )
+					.Where( c => c.GetCompatibleWithPlatform( BuildTarget.iOS ) )
+					.Select( c => AssetDatabase.GetAssetPath( c ) )
+					.ToArray()
+				;
+
+			if ( !list.Any() ) return;
+
+			var sb = new StringBuilder();
+
+			foreach ( var n in list )
+			{
+				sb.AppendLine( n );
+			}
+
+			Assert.Fail( sb.ToString() );
+		}
+	}
+}
